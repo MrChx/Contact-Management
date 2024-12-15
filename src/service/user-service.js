3ebf4915ada4d55aba6b38458d51c116afae0f7c
unknown <Achaqilal407@gmail.com>
2024-12-15 11:48:07 +0800

    complete API get user

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,5 +1,5 @@
 import { prismaClient } from "../application/database.js";
-import { loginUserValidation, registerUserValidation } from "../validation/user-validation.js"
+import { getUserValidation, loginUserValidation, registerUserValidation } from "../validation/user-validation.js"
 import { validate } from "../validation/validation.js"
 import { ResponseError } from "../error/response-error.js";
 import bcrypt from "bcrypt";
@@ -65,7 +65,28 @@ const login = async (request) => {
     });
 }
 
+const get =  async (username) => {
+    username = validate(getUserValidation, username);
+
+    const user = await prismaClient.user.findUnique({
+        where: {
+            username: username
+        },
+        select: {
+            username: true,
+            name: true
+        }
+    });
+
+    if(!user) {
+        throw new ResponseError(404, "User tidak ditemukan");
+    }
+
+    return user;
+}
+
 export default{
     register,
-    login
+    login,
+    get
 }
