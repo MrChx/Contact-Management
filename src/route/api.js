62ced17e212887e0ed3ef4f17bb0dc175ab8aa88
unknown <Achaqilal407@gmail.com>
2024-12-15 14:18:34 +0800

    complate API create contact for table contact

--- src/route/api.js
+++ src/route/api.js
@@ -1,12 +1,16 @@
 import express from "express";
 import userController from "../controller/user-controller.js";
 import { authMiddleware } from "../middleware/auth-middleware.js";
+import contactController from "../controller/contact-controller.js";
 
 const userRouter = new express.Router();
+
 userRouter.use(authMiddleware);
 userRouter.get('/api/users', userController.get);
 userRouter.patch('/api/users/update', userController.update);
 userRouter.delete('/api/users/logout', userController.logout);
+
+userRouter.post('/api/contact', contactController.create);
 
 export {
     userRouter
