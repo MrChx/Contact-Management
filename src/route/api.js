33fceb965dc8810f258090ae6e6b0475c9c64205
unknown <Achaqilal407@gmail.com>
2024-12-17 11:29:48 +0800

    complete API for address

--- src/route/api.js
+++ src/route/api.js
@@ -2,6 +2,7 @@ import express from "express";
 import userController from "../controller/user-controller.js";
 import { authMiddleware } from "../middleware/auth-middleware.js";
 import contactController from "../controller/contact-controller.js";
+import addressController from "../controller/address-controller.js";
 
 const userRouter = new express.Router();
 
@@ -17,6 +18,11 @@ userRouter.delete('/api/remove/contact/:contactId', contactController.remove);
 userRouter.get('/api/get/contact/search', contactController.search);
 userRouter.get('/api/get/contact', contactController.getAllData);
 
+userRouter.post('/api/contacts/:contactId/addresses', addressController.create);
+userRouter.get('/api/contacts/:contactId/addresses/:addressId', addressController.get);
+userRouter.put('/api/contacts/:contactId/addresses/:addressId', addressController.update);
+userRouter.delete('/api/contacts/:contactId/addresses/:addressId', addressController.remove);
+userRouter.get('/api/contacts/:contactId/addresses', addressController.list);
 
 export {
     userRouter
